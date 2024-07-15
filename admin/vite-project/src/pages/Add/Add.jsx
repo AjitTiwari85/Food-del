ffeb2b83c2a29bda2ecb0a2128e76e31aed2a16a
Rahul Tiwari <at856575@gmail.com>
2024-07-15 19:18:50 +0530

    changes

--- admin/vite-project/src/pages/Add/Add.jsx
+++ admin/vite-project/src/pages/Add/Add.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./Add.css";
 import { assets, url } from "../../assets/assets";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Add = () => {
+  const url = " http://localhost:4000";
   const [image, setImage] = useState(false);
   const [data, setDate] = useState({
     name: "",
@@ -17,13 +20,36 @@ const Add = () => {
     setDate((data) => ({ ...data, [name]: value }));
   };
 
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("price", Number(data.price));
+    formData.append("category", data.category);
+    formData.append("image", image);
+    const response = await axios.post(`${url}/api/food/add`, formData);
+    if (response.data.success) {
+      setDate({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad",
+      });
+      setImage(false);
+      toast.success(response.data.message);
+    } else {
+      toast.error(response.data.message);
+    }
+  };
+
   // useEffect(() => {
   //   console.log(data);
   // }, [data]);
 
   return (
     <div className="add">
-      <form className="flex-col">
+      <form className="flex-col" onSubmit={onSubmitHandler}>
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
