ff092b4ddc50dbc3c367a88c02b7546e288909f1
Rahul Tiwari <at856575@gmail.com>
2024-08-11 23:55:56 +0530

    changes

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import "./MyOrders.css";
 import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
+import { assets } from "../../assets/frontend_assets/assets";
 
 const MyOrders = () => {
   const { url, token } = useContext(StoreContext);
@@ -23,7 +24,35 @@ const MyOrders = () => {
     }
   }, [token]);
 
-  return <div>MyOrders</div>;
+  return (
+    <div className="my-orders">
+      <h2>My Orders</h2>
+      <div className="container">
+        {data.map((order, index) => {
+          return (
+            <div key={index} className="my-orders-order">
+              <img src={assets.parcel_icon} alt="" />
+              <p>
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + " x " + item.quantity;
+                  } else {
+                    return item.name + " x " + item.quantity + ", ";
+                  }
+                })}
+              </p>
+              <p>${order.amount}.00</p>
+              <p>Items:{order.items.length}</p>
+              <p>
+                <span>&#x25cf;</span> <b>{order.status}</b>
+              </p>
+              <button>Track Order</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default MyOrders;
